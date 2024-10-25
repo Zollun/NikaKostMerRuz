dedf72912965f66e62b5a766fb5ba8f88344fc0e
Zollun <132633802+Zollun@users.noreply.github.com>
2024-10-25 10:47:48 +0400

    Create index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+class Myclass:
+    """
+    Описание класса MyClass
+    """
+  def __init__(self,name):
+    """
+    Иницализация клсса
+    """
+    self.name = name
+
+  def say.hello(self):
+  """
+  Метод для приветствия 
+  """
+  print(f"Привет,я {self.name}!)
